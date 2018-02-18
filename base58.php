e9bb0a4517206125e34e82d0bba9ad04192a9c57
sneurlax <sneurlax@gmail.com>
2018-02-17 21:59:09 -0600

    stub functions

---
+++ base58.php
@@ -0,0 +1,29 @@
+<?php
+
+class base58 {
+  /**
+   *
+   * Encode a hexadecimal (Base16) input to Base58
+   *
+   * @param    string  $input  A hexadecimal (Base16) input to convert to Base58
+   * @return   string
+   *
+   */
+  public function encode($input) {
+    // stub
+  }
+
+  /**
+   *
+   * Decode a Base58 input to hexadecimal (Base16)
+   *
+   * @param    string  $input  A Base58 input to convert to hexadecimal (Base16)
+   * @return   string
+   *
+   */
+  public function decode($input) {
+    // stub
+  }
+}
+
+?>
