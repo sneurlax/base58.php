bef99383246baee4b18f60f272e0629230b4bbc4
sneurlax <sneurlax@gmail.com>
2018-02-21 19:56:25 -0600

    better description, attribution, copyright/license (none,) and examples

--- base58.php
+++ base58.php
@@ -1,11 +1,33 @@
 <?php
-/*
+/**
+ *
  * base58.php
  *
- * PHP Base58 codec
+ * PHP Base58 codec for converting between hexadecimal (Base16) and Base58
+ * For use with Cryptonote- and/or Bitcoin-based currencies
  *
- * Based on https://github.com/MoneroPy/moneropy/base58.py and https://github.com/mymonero/mymonero-core-js/cryptonote_utils/cryptonote_base58.js
+ * Using work from
+ *   https://github.com/MoneroPy/moneropy/base58.py
+ *   https://github.com/mymonero/mymonero-core-js/cryptonote_utils/cryptonote_base58.js
  *
+ * @author     sneurlax <sneurlax@gmail.com>
+ * @copyright  2018
+ * @license    Public Domain
+ * 
+ * ============================================================================
+ * 
+ * // Initialize base58 class
+ * include('base58.php');
+ * $base58 = new Base58;
+ * 
+ * // Encode a string
+ * $base58->encode('129f5a5c1545dc3a1db567154121878f08f8572cdf45e5549c624fb3f01fbd274690716e09edf5658cb0c2be87e067149ff6ccdbe6a909eeb65db22a8e6d2eb5ce3f8c3d80');
+ *   // Should equal '47fMeiDeVkd5yDDiDZY4RU2W2Dh8GyAuyTA81xDwAnyUCoZZex6SEWgQXsgVrYvFk7TkrQSFcqBBKXWBMwi3sDzCQGciKFM'
+ * 
+ * // Decode a string
+ * $base58->decode('47fMeiDeVkd5yDDiDZY4RU2W2Dh8GyAuyTA81xDwAnyUCoZZex6SEWgQXsgVrYvFk7TkrQSFcqBBKXWBMwi3sDzCQGciKFM');
+ *   // Should equal '129f5a5c1545dc3a1db567154121878f08f8572cdf45e5549c624fb3f01fbd274690716e09edf5658cb0c2be87e067149ff6ccdbe6a909eeb65db22a8e6d2eb5ce3f8c3d80'
+ * 
  */
 
 class base58 {
